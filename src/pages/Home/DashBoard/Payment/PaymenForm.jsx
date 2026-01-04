1e1be77e40a9451471b838d1d1057f9584f93d82
Hossain3227 <hasan32.hk27@gmail.com>
2026-01-05 03:59:03 +0600

    bug fixed

--- src/pages/Home/DashBoard/Payment/PaymenForm.jsx
+++ src/pages/Home/DashBoard/Payment/PaymenForm.jsx
@@ -1,12 +1,13 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useState } from 'react';
 
 const PaymenForm = () => {
 
     const stripe = useStripe();
   const elements = useElements();
+  const [error,setError] = useState('');
 
-    const handleSubmit = e =>{
+    const handleSubmit = async(e) =>{
         e.preventDefault();
         if(!stripe|| !elements){
             return;
@@ -15,16 +16,38 @@ const PaymenForm = () => {
     if(!card){
         return;
     }
+
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+        type: 'card',
+        card
+    })
+    if(error){
+        // console.log('error',error);
+        setError(error.message);
+        
+    }
+    else{
+        setError('');
+        console.log('payment method', paymentMethod);
+        
+    }
+
   }
 
 
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-            <CardElement>
-                <button type='submit' disabled={!stripe}>Pay for parcel pick up</button>
-            </CardElement>
+            <form onSubmit={handleSubmit} className='space-y-4 bg-white p-6 rounded-xl shadow-md w-full max-w-md mx-auto'>
+            <CardElement className='p-2 border rounded'>
+                </CardElement>
+                <button
+                className='btn btn-primary w-full'
+                type='submit' disabled={!stripe}>Pay for parcel pick up</button>
+                {
+                    error && <p className='text-red-500'>{error}</p>
+                }
+            
             </form>
         </div>
     );
